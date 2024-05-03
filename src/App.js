f5fa05195253af3c128173f20847c2eb9fb3619a
/OUSHI33R <98487768+OUSHI33R@users.noreply.github.com>
2024-05-03 11:39:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Home/>
+      <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/5ef41f7483fc5b39fc99b2cc/script.js"></script> 
     </div>
   );
 }
