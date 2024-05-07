315f36d3c99fee71f7db872fca55235b05c9e060
/OUSHI33R <98487768+OUSHI33R@users.noreply.github.com>
2024-05-07 23:06:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <Home/>
-      <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/5ef41f7483fc5b39fc99b2cc/script.js"></script> 
       <script type="text/javascript" src="https://app.termly.io/resource-blocker/5696ec9c-bd94-427c-9b4e-673bb11bc4d2?autoBlock=on"></script>
     </div>
   );
