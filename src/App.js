8c2cc49a47fab0d33a2c9eb1791c3acb77d9ad71
/OUSHI33R <98487768+OUSHI33R@users.noreply.github.com>
2024-05-14 12:46:35 +0100

    Add GA4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Home from "./Page/Home";
+import ReactGA4 from 'react-ga4';
 
 
 function App() {
+  ReactGA4.init('G-5XYY30EH3Q'); // Initialize with your GA4 property ID
+  ReactGA4.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
-      <Home/>
+      <Home />
       <script type="text/javascript" src="https://app.termly.io/resource-blocker/5696ec9c-bd94-427c-9b4e-673bb11bc4d2?autoBlock=on"></script>
     </div>
   );
